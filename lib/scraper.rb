efd8e9b760e399b3049e548e86214c09c1832079
Audrea Cook <audreacwork@gmail.com>
2019-08-01 20:12:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
@@ -14,6 +14,10 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+  end
+  
+  def get_page
+    doc = Nokogiri::HTML(open(''))
   end
   
 end
